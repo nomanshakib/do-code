6014db9bed419a3015784afbe0cd8ea111fc4e4c
nomanshakib <nomanshakib.official@gmail.com>
2022-09-30 17:04:39 +0600

    answer 1

--- src/components/QuestionAnswer/QuestionAnswer.js
+++ src/components/QuestionAnswer/QuestionAnswer.js
@@ -12,14 +12,14 @@ const QuestionAnswer = () => {
                     </h2>
                     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                    <p> </p>
+                    <p>React js work is usually virtual dom create. React components and their JSX are no different from other template libraries. For instance, Handlebars will render a template to HTML markup as a string, which is then inserted into the browser DOM. Where React is different from libraries like Handlebars is when data changes, and we need to re-render the component. Handlebars will just rebuild the entire HTML string, the same way it did on the initial render. Since this is problematic for performance, we often end up implementing imperative workarounds that manually update tiny bits of the DOM. What we end up with is a tangled mess of declarative templates, and imperative code to handle the dynamic aspects of the UI.</p>
                     </div>
                     </div>
                 </div>
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingTwo">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        <h3>Question-2. Difference Props VS State?</h3>
+                        <h3>Question-2. Props VS State difference?</h3>
                     </button>
                     </h2>
                     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
